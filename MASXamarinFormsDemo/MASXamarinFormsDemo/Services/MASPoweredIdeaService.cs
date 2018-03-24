b8bc0e3c9f4e797b95525af2f1611f1c05232792
Auri A. Rahimzadeh <auri@auri.net>
2018-03-24 17:23:57 -0400

    Added placeholder MAS-Powered Idea Service. To be filled in with our MAS implementation.

---
+++ MASXamarinFormsDemo/MASXamarinFormsDemo/Services/MASPoweredIdeaService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MASXamarinFormsDemo.Models;
+
+namespace MASXamarinFormsDemo.Services
+{
+    public class MASPoweredIdeaService : IIdeaService<Idea>
+    {
+
+        #region Interface Required Items
+
+        public bool IsAuthenticated { get; set; }
+
+        /// <inheritdoc />
+        public Task<bool> LogOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> AddIdeaAsync(Idea item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> UpdateIdeaAsync(Idea item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> DeleteIdeaAsync(Idea item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<Idea> GetIdeaAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<IEnumerable<Idea>> GetIdeasAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
